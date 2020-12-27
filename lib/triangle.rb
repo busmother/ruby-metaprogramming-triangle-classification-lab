5a9ee7614cda806ed6ec2d7f5fc3c0810bcf1cf5
Ian Marshall <imarshall14@gmail.com>
2020-12-27 11:19:10 -0500

    commit tree f80921d6eb8f534720fa3e79137d56a200218a99

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def equilateral?(a,b,c)
+    ((a == b) && (a == c) && (b == c)) ? true : false
+  end
+
+  def isosceles?(a,b,c)
+    ((a == b) || (a == c) || (b == c)) ? true : false
+  end
+
+  def triangleinequality
+    ((a + b <= c) || (b + c <= a) || (c + a <= b)) ? true : false
+  end
+
+  def zerolength(a,b,c)
+    ((a == 0) || (b == 0) || (c == 0)) ? true : false
+  end
+
+  def kind
+    if self.triangleinequality == true
+      raise TriangleError
+      puts error.message
+    elsif zerolength(@a,@b,@c) == true
+      raise TriangleError
+      puts error.message
+    elsif equilateral?(@a,@b,@c) == true
+      return :equilateral
+    elsif isosceles?(@a,@b,@c) == true
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "That's not a triangle, love."
+    end
+  end
 end
